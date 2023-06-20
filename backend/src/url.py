80343ca4796e21179cbc0a377984d82fef671ed3
Thomas Palakapilly <thomaspalakapilly@gmail.com>
2023-06-19 19:37:01 -0500

    implemented everything

---
+++ backend/src/url.py
@@ -0,0 +1,78 @@
+from flask import Blueprint, request, send_file
+import base64
+import requests
+from PIL import Image
+from rembg import new_session, remove
+import boto3
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="../.env")
+
+s3_client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
+    region_name=os.getenv('AWS_REGION_NAME'))
+
+
+bp = Blueprint('url', __name__, url_prefix='/yo')
+
+
+@bp.route('/', methods=['POST'])
+def link_submit():
+   data = request.json
+   print(data)
+   img_data = requests.get(data['url']).content
+   ext = find_extension(data['url'])
+   with open(f"preprocessed/{data['name']}{ext}", 'wb') as handler:
+        handler.write(img_data)
+        return {'success':True}, 200, {'ContentType':'application/json'}
+   
+@bp.route('/batch-process', methods=['GET'])
+def batch_process():
+    file_names = os.listdir('preprocessed')
+    try:
+        for thing in file_names:
+            input_path = f"preprocessed/{thing}"
+            output_path = f"postprocessed/{thing[0:find_dot(thing)]}.png"
+            input = Image.open(input_path)
+            output = remove(input)
+            output.save(output_path)
+    except:
+        return {'success':False}, 400, {'ContentType':'application/json'}
+    return {'success':True}, 200, {'ContentType':'application/json'}
+
+
+@bp.route('/batch-upload', methods=['GET'])
+def batch_upload():
+    file_names = os.listdir('postprocessed')
+    try:
+        for thing in file_names:
+            path = f"postprocessed/{thing}"
+            upload_file(path, thing)
+    except:
+        return {'success':False}, 400, {'ContentType':'application/json'}
+    return {'success':True}, 200, {'ContentType':'application/json'}
+
+def find_dot(slug):
+    dot = 0
+    for index in reversed(range(0, len(slug))):
+        if slug[index] == '.':
+            dot = index
+            break
+    return dot
+   
+
+def find_extension(slug):
+    return slug[find_dot(slug):]
+
+
+def upload_file(file_path, file_name):
+    try:
+        response = s3_client.upload_file(file_path, os.getenv('AWS_BUCKET_NAME'), f"logos/bot/{file_name}")
+        print(response)
+    except ClientError as e:
+        print(e)
+        return False
+    return True
+    
---
+++ frontend/src/App.svelte
@@ -0,0 +1,110 @@
+<script>
+  let error = false;
+  let lig = '';
+  let urlField;
+  let compName = '';
+  let previewImg;
+
+  async function onSubmit(e) {
+    console.log(lig)
+    if(lig == '' || compName == ''){
+      console.log('no url')
+      urlField.focus()
+      error = true
+    } else{
+        var res = await fetch(`http://127.0.0.1:5000/yo/`, {
+          method: 'POST',
+          body: JSON.stringify({
+            url: lig,
+            name: compName
+          }),
+          headers: {
+		        'Content-type': 'application/json; charset=UTF-8'
+	        }
+        })
+        previewImg = lig;
+        lig = '';
+        compName = '';
+        urlField.focus()
+        //console.log(res.headers.get('Content-Disposition'))
+    }
+  }
+
+  function typed(e){
+    if(lig != '')
+      error = false
+    else error = true;  
+  }
+
+  async function batchProcess(e){
+    if(!confirm('WARNING: this will probably take a while.'))
+      return    
+    console.log(await fetch('http://127.0.0.1:5000/yo/batch-process', {
+      method: 'GET'
+    }))
+  }
+
+  async function batchUpload(e){
+    if(!confirm('WARNING: this will delete all imgs from postprocessed dir.'))
+      return    
+    console.log(await fetch('http://127.0.0.1:5000/yo/batch-upload', {
+      method: 'GET'
+    }))
+  }
+
+</script>
+
+<main>
+  <div class='container'>
+    <div class="card" id='form'>
+      <form on:submit|preventDefault={onSubmit}>
+        <input style="!important margin-bottom: 8px" bind:this={urlField} on:change={typed} bind:value={lig} name='lig' id='url' type='text' placeholder="enter image URL">
+        <input on:change={typed} bind:value={compName} name='name' id='url' type='text' placeholder="enter company name">
+          <div style="padding-top: 4%; padding-bottom: 4%; margin-top: 8%; margin-bottom: 8%;">
+            {#if error}
+            <p class="error-message">enter url & name</p>
+            {/if}
+          </div>       
+        <button type='submit' style='margin-top: 0%'>submit</button>
+        <br>
+        <button on:click|preventDefault={batchProcess} style='margin-top: 30%; color: white; background-color: green;'>batch process</button>
+        <button on:click|preventDefault={batchUpload} style='margin-top: 30%; color: white; background-color: red;'>batch upload</button>
+      </form>
+    </div>
+    <div class="card" id='preview'>
+      <img src={previewImg} style="width: 300px; height: auto;" alt="Vite Logo" />
+    </div>
+  </div>
+    
+</main>
+
+<style>
+  .container{
+    display: flex;
+  }
+
+  #form{
+    padding-right: 500px;
+    flex: 30%;
+  }
+
+  #preview{
+    /* margin-left: 80%; */
+    flex: 70%;
+  }
+
+  #url{
+    padding: 10% 2% 10% 5%;
+    margin-bottom: 25px;
+    width: auto;
+    font-size: large;
+  }
+
+  .error-message {
+    color: tomato;
+    font-size: 1.2em;
+    margin: 0%;
+    padding: 0;
+  }
+
+</style>
